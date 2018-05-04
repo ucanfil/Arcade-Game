81295c9fe11b0edeea6f480c625a1dc9658f93ad
Burak Tilek <burak.tilek@gmail.com>
2018-05-04 21:57:01 +0300

    Enemy sprite moves off screen, stops off screen

--- js/app.js
+++ js/app.js
@@ -1,13 +1,15 @@
 // Enemies our player must avoid
-const Enemy = function() {
+const Enemy = function (x = -100, y = 143, width = 101, height = 171) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.width = width;
+    this.height = height;
     this.x = x;
-    this.y = y;
-    this.speed = 1;
+    this.y = 143 + Math.floor(Math.random() * 3) * 83;
+    this.speed = Math.max(100, Math.floor(Math.random() * 5) * 100);
     this.sprite = 'images/enemy-bug.png';
 };
 
@@ -17,13 +19,24 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (this.x > 505 + this.width) {
+      return;
+    }
     this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  ctx.drawImage(Resources.get(this.sprite), this.x - this.width / 2, this.y - this.height / 2);
 };
+
+const level = 'easy';
+const numEnemies = level === 'hard' ? 12 : level === 'medium' ? 9 : 6;
+
+let enemy;
+const allEnemies = [];
+enemy = new Enemy();
+allEnemies.push(enemy);
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -63,7 +76,6 @@ Player.prototype.handleInput = function (direction) {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-const allEnemies = [];
 const player = new Player(252.5, 456.5);
 
 

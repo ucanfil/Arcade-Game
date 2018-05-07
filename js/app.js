3bd92630e5763817c078fcfc33b7d83d9f1c3169
Burak Tilek <burak.tilek@gmail.com>
2018-05-07 12:14:43 +0300

    Player update method implemented

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-// Enemies our player must avoid
+// Our base class for game entitites
 class GameEntities {
-  constructor(x, y, width = 101, height = 171, sprite = 'images/enemy-bug.png') {
+  constructor(x, y, width = 70, height = 88, sprite = 'images/enemy-bug.png') {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -9,32 +9,34 @@ class GameEntities {
   };
 
   render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x - this.width / 2, this.y - this.height / 2);
+    ctx.drawImage(Resources.get(this.sprite), this.x - this.width / 2, this.y);
   };
 };
 
+// Enemy class inherits from GameEntities
 class Enemy extends GameEntities {
-  constructor(x, y, width, height, sprite, speed = 100, num = 5) {
+  constructor(x, y, width = 100, height = 79, sprite, speed = 100, num = 5) {
     super(x, y, width, height, sprite);
-    this.x = -100;
-    this.y = 143 + Math.floor(Math.random() * 3) * 83;
+    this.speed = speed;
     this.num = num;
-    this.speed = Math.max(Math.floor(Math.random() * (num - 3)) * 100, Math.floor(Math.random() * num) * 100);
   }
 
   render() {
     super.render();
-    ctx.drawImage(Resources.get(this.sprite), this.x - this.width / 2, this.y - this.height / 2);
+    ctx.drawImage(Resources.get(this.sprite), this.x - this.width / 2, this.y);
   };
 
   update(dt) {
     if (this.x > 505 + this.width) {
-      this.x = -100;
-    }
+      this.x = -100 - Math.floor(Math.random() * this.num) * 100;
+      this.y = 135 + Math.floor(Math.random() * this.num) * 83;
+      this.speed = Math.max(100, Math.floor(Math.random() * this.num) * 100);
+    };
     this.x += this.speed * dt;
   }
 }
 
+// Player class inherits from GameEntities
 class Player extends GameEntities {
   constructor(x = 252.5, y = 456.5, width, height, sprite) {
     super(x, y, width, height, sprite);
@@ -42,11 +44,20 @@ class Player extends GameEntities {
   }
 
   update(dt) {
+    const item = this; // Neded this for closure!
+    const tolerance = 25;
+    allEnemies.forEach(function(enemy) {
+      if (item.x < enemy.x + enemy.width - tolerance && item.x + item.width - tolerance > enemy.x &&
+        item.y < enemy.y + enemy.height - tolerance && item.height - tolerance + item.y > enemy.y) {
+          item.x = 252.5;
+          item.y = 465;
+      };
+    });
 
   };
 
   render() {
-    ctx.drawImage(Resources.get(this.sprite), this.x - this.width / 2, this.y - this.height / 2.5);
+    ctx.drawImage(Resources.get(this.sprite), this.x - this.width / 2, this.y);
   }
 
   handleInput(direction) {
@@ -68,98 +79,23 @@ class Player extends GameEntities {
 };
 
 
-
-/* const Enemy = function (x = -100, y = 143, width = 101, height = 171) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = 143 + Math.floor(Math.random() * 3) * 83;
-    this.speed = Math.max(Math.floor(Math.random() * 2) * 100, Math.floor(Math.random() * 5) * 100);
-    this.sprite = 'images/enemy-bug.png';
-};
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    if (this.x > 505 + this.width) {
-      this.x = -100;
-    }
-    this.x += this.speed * dt;
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x - this.width / 2, this.y - this.height / 2);
-}; */
-
 const level = 'easy';
 const numEnemies = level === 'hard' ? 12 : level === 'medium' ? 9 : 6;
 
+
+const numOfEnemies = 2;
+let enemy;
 const allEnemies = [];
 
-for (let i = 0; i < 6; i++) {
-  num = 6;
-  randomX = -100 - Math.floor(Math.random() * 3) * 100;
-  randomY = 143 + Math.floor(Math.random() * 3) * 83;
-
-  speed = Math.max(Math.floor(Math.random() * (num - 3)) * 100, Math.floor(Math.random() * num) * 100);
-  let enemy = new Enemy(randomX, randomY, speed, num);
+for (let i = 0; i < numOfEnemies; i++) {
+  enemy = new Enemy(-100, 135, 100, 79, 'images/enemy-bug.png', 100, 3);
   allEnemies.push(enemy);
 }
-/*
-constructor(x, y, width, height, sprite, speed = 100, num = 5)
-this.x = -100;
-this.y = 143 + Math.floor(Math.random() * 3) * 83;
-this.num = num;
-this.speed = Math.max(Math.floor(Math.random() * (num - 3)) * 100, Math.floor(Math.random() * num) * 100); */
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-/* const Player = function(x, y, width = 101, height = 171) {
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
-    this.sprite = 'images/char-boy.png';
-};
-
-Player.prototype.update = function(dt) {
-};
-
-Player.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x - this.width / 2, this.y - this.height / 2.5);
-};
-
-Player.prototype.handleInput = function (direction) {
-    switch (direction) {
-      case 'left':
-        this.x > 50.5 ? this.x -= 101 : this.x;
-        break;
-      case 'right':
-        this.x < 454.5 ? this.x += 101 : this.x;
-        break;
-      case 'up':
-        this.y > 41.5 ? this.y -= 83 : this.y;
-        break;
-      case 'down':
-        this.y < 456.5 ? this.y += 83 : this.y;
-        break;
-    };
-}; */
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-const player = new Player(252.5, 456.5);
+const player = new Player(252.5, 465);
 
 
 

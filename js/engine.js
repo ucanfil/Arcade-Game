e35561b0eee3192ce35947768a597d9160db9cb2
Burak Tilek <burak.tilek@gmail.com>
2018-05-07 13:58:54 +0300

    Gem update method added into updateEntities function

--- js/engine.js
+++ js/engine.js
@@ -94,6 +94,7 @@ var Engine = (function(global) {
             enemy.update(dt);
         });
         player.update();
+        gem.update();
     }
 
     /* This function initially draws the "game level", it will then call
@@ -154,6 +155,7 @@ var Engine = (function(global) {
         });
 
         player.render();
+        gem.render();
     }
 
     /* This function does nothing but it could have been a good place to

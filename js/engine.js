e948f48ecbd2c07005cf6b3fdd69aa1666ebb351
Burak Tilek <burak.tilek@gmail.com>
2018-05-07 22:58:43 +0300

    Added gem and heart update methods into updateEntities function

--- js/engine.js
+++ js/engine.js
@@ -95,6 +95,7 @@ var Engine = (function(global) {
         });
         player.update();
         gem.update();
+        heart.update();
     }
 
     /* This function initially draws the "game level", it will then call
@@ -156,6 +157,7 @@ var Engine = (function(global) {
 
         player.render();
         gem.render();
+        heart.render();
     }
 
     /* This function does nothing but it could have been a good place to
